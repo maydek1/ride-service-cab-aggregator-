4effdc4bc911e7284848dafdc71e15b25141efc0
maydek1 <maydekcraft@gmail.com>
2024-10-02 15:02:48 +0300

    feat(circuit-breaker): integrate Resilience4j for Circuit Breaker functionality
    - Implemented Circuit Breaker using Resilience4j for enhanced resilience
    - Configured fallback mechanisms and error handling strategies

--- src/main/java/com/software/modsen/rideservice/client/DriverClient.java
+++ src/main/java/com/software/modsen/rideservice/client/DriverClient.java
@@ -3,17 +3,20 @@ package com.software.modsen.rideservice.client;
 import com.software.modsen.rideservice.config.FeignConfig;
 import com.software.modsen.rideservice.dto.request.DriverRatingRequest;
 import com.software.modsen.rideservice.dto.request.DriverResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "${driver-service.name}", path = "${driver-service.path}" ,configuration = FeignConfig.class)
+@FeignClient(name = "${driver-service.name}", path = "${driver-service.path}" ,configuration = FeignConfig.class, fallback = DriverClientFallback.class)
 public interface DriverClient {
+    @CircuitBreaker(name = "driverService")
     @PostMapping("/rating")
     ResponseEntity<DriverResponse> updateRating(@RequestBody DriverRatingRequest driverRatingRequest);
 
+    @CircuitBreaker(name = "driverService")
     @PostMapping("/available/{id}/{available}")
     ResponseEntity<DriverResponse> toggleAvailable(@PathVariable Long id, @PathVariable boolean available);
 }
--- src/main/java/com/software/modsen/rideservice/client/PassengerClient.java
+++ src/main/java/com/software/modsen/rideservice/client/PassengerClient.java
@@ -3,13 +3,15 @@ package com.software.modsen.rideservice.client;
 
 import com.software.modsen.rideservice.config.FeignConfig;
 import com.software.modsen.rideservice.dto.response.PassengerResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "${passenger-service.name}", path = "${passenger-service.path}", configuration = FeignConfig.class)
+@FeignClient(name = "${passenger-service.name}", path = "${passenger-service.path}", configuration = FeignConfig.class, fallback = PassengerClientFallback.class)
 public interface PassengerClient {
+    @CircuitBreaker(name = "passengerService")
     @GetMapping("/{id}")
     ResponseEntity<PassengerResponse> getPassengerById(@PathVariable Long id);
 }
