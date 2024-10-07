d93759ca6999097a3e81407bfb164f26a0452696
maydek1 <maydekcraft@gmail.com>
2024-10-07 11:13:27 +0300

    test(integration): controller unit test changed

--- src/test/java/com/software/modsen/rideservice/controller/RideControllerTest.java
+++ src/test/java/com/software/modsen/rideservice/controller/RideControllerTest.java
@@ -1,10 +1,11 @@
 package com.software.modsen.rideservice.controller;
 
-import com.software.modsen.rideservice.dto.request.RideRequest;
+import com.software.modsen.rideservice.dto.request.RideDriverRequest;
+import com.software.modsen.rideservice.dto.request.RideStatusRequest;
 import com.software.modsen.rideservice.dto.response.RideResponse;
-import com.software.modsen.rideservice.dto.response.RideResponseSet;
 import com.software.modsen.rideservice.mapper.RideMapper;
 import com.software.modsen.rideservice.model.Ride;
+import com.software.modsen.rideservice.model.RideStatus;
 import com.software.modsen.rideservice.service.DriverService;
 import com.software.modsen.rideservice.service.RideService;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,20 +13,20 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.Collections;
-import java.util.Objects;
+import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class RideControllerTest {
-
-    @InjectMocks
-    private RideController rideController;
 
     @Mock
     private RideService rideService;
@@ -36,75 +37,192 @@ class RideControllerTest {
     @Mock
     private RideMapper rideMapper;
 
-    private RideRequest rideRequest;
+    @InjectMocks
+    private RideController rideController;
+
+    private MockMvc mockMvc;
+
+    private Ride ride;
     private RideResponse rideResponse;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
-        rideRequest = new RideRequest();
+        mockMvc = MockMvcBuilders.standaloneSetup(rideController).build();
+
+        ride = new Ride();
+        ride.setId(1L);
+        ride.setStatus(RideStatus.CREATED);
+
         rideResponse = new RideResponse();
+        rideResponse.setId(1L);
+        rideResponse.setStatus(RideStatus.CREATED);
     }
 
     @Test
-    void createRide_ShouldReturnCreatedRide() {
-        when(rideMapper.rideRequestToRide(any(RideRequest.class))).thenReturn(new Ride());
-        when(rideService.createRide(any(Ride.class))).thenReturn(new Ride());
-        when(rideMapper.rideToRideResponse(any(Ride.class))).thenReturn(rideResponse);
+    void createRide_ShouldReturnCreatedRide_WhenValidRequest() throws Exception {
+        when(rideService.createRide(any(Ride.class))).thenReturn(ride);
+        when(rideMapper.rideToRideResponse(ride)).thenReturn(rideResponse);
+        when(rideMapper.rideRequestToRide(any())).thenReturn(ride);
+        mockMvc.perform(post("/api/v1/ride")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"passengerId\": 123, \"driverId\": 456}"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.status").value("CREATED"));
 
-        ResponseEntity<RideResponse> responseEntity = rideController.createRide(rideRequest);
-
-        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
-        assertEquals(rideResponse, responseEntity.getBody());
         verify(rideService, times(1)).createRide(any(Ride.class));
     }
 
     @Test
-    void getRideById_ShouldReturnRide() {
-        when(rideService.getRideById(anyLong())).thenReturn(new Ride());
-        when(rideMapper.rideToRideResponse(any(Ride.class))).thenReturn(rideResponse);
+    void getRideById_ShouldReturnRideResponse_WhenRideExists() throws Exception {
+        when(rideService.getRideById(1L)).thenReturn(ride);
+        when(rideMapper.rideToRideResponse(ride)).thenReturn(rideResponse);
 
-        ResponseEntity<RideResponse> responseEntity = rideController.getRideById(1L);
+        mockMvc.perform(get("/api/v1/ride/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.status").value("CREATED"));
 
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals(rideResponse, responseEntity.getBody());
         verify(rideService, times(1)).getRideById(1L);
     }
 
     @Test
-    void updateRide_ShouldReturnUpdatedRide() {
-        when(rideMapper.rideRequestToRide(any(RideRequest.class))).thenReturn(new Ride());
-        when(rideService.updateRide(anyLong(), any(Ride.class))).thenReturn(new Ride());
-        when(rideMapper.rideToRideResponse(any(Ride.class))).thenReturn(rideResponse);
+    void updateRide_ShouldReturnUpdatedRide_WhenValidRequest() throws Exception {
+        when(rideService.updateRide(eq(1L), any(Ride.class))).thenReturn(ride);
+        when(rideMapper.rideToRideResponse(ride)).thenReturn(rideResponse);
+        when(rideMapper.rideRequestToRide(any())).thenReturn(ride);
+        mockMvc.perform(put("/api/v1/ride/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"passengerId\": 123, \"driverId\": 456}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.status").value("CREATED"));
 
-        ResponseEntity<RideResponse> responseEntity = rideController.updateRide(1L, rideRequest);
-
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals(rideResponse, responseEntity.getBody());
         verify(rideService, times(1)).updateRide(eq(1L), any(Ride.class));
     }
 
     @Test
-    void deleteRideById_ShouldReturnDeletedRide() {
-        when(rideService.deleteRideById(anyLong())).thenReturn(new Ride());
-        when(rideMapper.rideToRideResponse(any(Ride.class))).thenReturn(rideResponse);
+    void deleteRideById_ShouldReturnDeletedRide_WhenRideExists() throws Exception {
+        when(rideService.deleteRideById(1L)).thenReturn(ride);
+        when(rideMapper.rideToRideResponse(ride)).thenReturn(rideResponse);
 
-        ResponseEntity<RideResponse> responseEntity = rideController.deleteRideById(1L);
+        mockMvc.perform(delete("/api/v1/ride/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.status").value("CREATED"));
 
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals(rideResponse, responseEntity.getBody());
         verify(rideService, times(1)).deleteRideById(1L);
     }
 
     @Test
-    void getAllRides_ShouldReturnRides() {
-        when(rideService.getAllRides()).thenReturn(Collections.singleton(new Ride()));
-        when(rideMapper.rideToRideResponse(any(Ride.class))).thenReturn(rideResponse);
+    void getAllRides_ShouldReturnSetOfRides() throws Exception {
+        when(rideService.getAllRides()).thenReturn(Set.of(ride));
+        when(rideMapper.rideToRideResponse(ride)).thenReturn(rideResponse);
 
-        ResponseEntity<RideResponseSet> responseEntity = rideController.getAllRides();
+        mockMvc.perform(get("/api/v1/ride"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.rides[0].id").value(1L))
+                .andExpect(jsonPath("$.rides[0].status").value("CREATED"));
 
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals(1, Objects.requireNonNull(responseEntity.getBody()).getRides().size());
         verify(rideService, times(1)).getAllRides();
+    }
+
+    @Test
+    void getRidesByDriver_ShouldReturnRides_WhenValidDriverId() throws Exception {
+        when(rideService.getRidesByDriver(1L)).thenReturn(Set.of(ride));
+        when(rideMapper.rideToRideResponse(ride)).thenReturn(rideResponse);
+
+        mockMvc.perform(get("/api/v1/ride/driver/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.rides[0].id").value(1L))
+                .andExpect(jsonPath("$.rides[0].status").value("CREATED"));
+
+        verify(rideService, times(1)).getRidesByDriver(1L);
+    }
+
+    @Test
+    void getRidesByPassenger_ShouldReturnRides_WhenValidPassengerId() throws Exception {
+        when(rideService.getRidesByPassenger(1L)).thenReturn(Set.of(ride));
+        when(rideMapper.rideToRideResponse(ride)).thenReturn(rideResponse);
+
+        mockMvc.perform(get("/api/v1/ride/passenger/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.rides[0].id").value(1L))
+                .andExpect(jsonPath("$.rides[0].status").value("CREATED"));
+
+        verify(rideService, times(1)).getRidesByPassenger(1L);
+    }
+
+    @Test
+    void getRidesByStatus_ShouldReturnRides_WhenValidStatus() throws Exception {
+        when(rideService.getRidesByStatus(RideStatus.CREATED)).thenReturn(Set.of(ride));
+        when(rideMapper.rideToRideResponse(ride)).thenReturn(rideResponse);
+
+        mockMvc.perform(get("/api/v1/ride/status/CREATED"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.rides[0].id").value(1L))
+                .andExpect(jsonPath("$.rides[0].status").value("CREATED"));
+
+        verify(rideService, times(1)).getRidesByStatus(RideStatus.CREATED);
+    }
+
+    @Test
+    void changeRideStatus_ShouldReturnUpdatedStatus_WhenValidRequest() throws Exception {
+        when(rideService.changeStatus(any(RideStatusRequest.class))).thenReturn(ride);
+        when(rideMapper.rideToRideResponse(ride)).thenReturn(rideResponse);
+
+        mockMvc.perform(put("/api/v1/ride/status")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"rideId\": 1, \"status\": \"ACCEPTED\"}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.status").value("CREATED"));
+
+        verify(rideService, times(1)).changeStatus(any(RideStatusRequest.class));
+    }
+
+    @Test
+    void setDriver_ShouldReturnUpdatedRide_WhenValidRequest() throws Exception {
+        when(rideService.setDriver(any(RideDriverRequest.class))).thenReturn(ride);
+        when(rideMapper.rideToRideResponse(ride)).thenReturn(rideResponse);
+
+        mockMvc.perform(put("/api/v1/ride/driver")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"rideId\": 1, \"driverId\": 2}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.status").value("CREATED"));
+
+        verify(rideService, times(1)).setDriver(any(RideDriverRequest.class));
+    }
+
+    @Test
+    void getRideToConfirm_ShouldReturnRidesToConfirm_WhenValidId() throws Exception {
+        when(rideService.getRidesToConfirm(1L)).thenReturn(Set.of(ride));
+        when(rideMapper.rideToRideResponse(ride)).thenReturn(rideResponse);
+
+        mockMvc.perform(get("/api/v1/ride/created/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.rides[0].id").value(1L))
+                .andExpect(jsonPath("$.rides[0].status").value("CREATED"));
+
+        verify(rideService, times(1)).getRidesToConfirm(1L);
+    }
+
+    @Test
+    void updateRideStatus_ShouldReturnUpdatedRideStatus_WhenValidRequest() throws Exception {
+        when(rideService.updateRideStatus(any(RideStatusRequest.class))).thenReturn(ride);
+        when(rideMapper.rideToRideResponse(ride)).thenReturn(rideResponse);
+        when(rideMapper.rideRequestToRide(any())).thenReturn(ride);
+
+        mockMvc.perform(post("/api/v1/ride/update-status")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"rideId\": 1, \"status\": \"ACCEPTED\"}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.status").value("CREATED"));
+
+        verify(rideService, times(1)).updateRideStatus(any(RideStatusRequest.class));
     }
 }
